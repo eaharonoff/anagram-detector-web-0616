e4270e30e11c247f5e25f162831e5e01c5a03843
eaharonoff <emanuel.aharonoff@gmail.com>
2016-06-10 14:08:38 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  def initialize (word)
+    @word = word 
+    @word_array = []
+  end
+
+  def match(word_array)
+    @word_array = word_array
+    @word_array.select{|word| @word.split('').sort == word.split('').sort}
+  end
+
+end
+
+#hey = Anagram.new("hey")
+
+#hey.match(%w(hye sup uo))
